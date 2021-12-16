97efc880bfb195d3bd554bec6b0ddb673fdffe8c
vonweinkeller <caiankeller@icloud.com>
2021-12-16 18:48:07 -0300

    fixing a variable

--- src/controllers/BookController.js
+++ src/controllers/BookController.js
@@ -10,10 +10,10 @@ module.exports = {
     const needValues = ["title", "author", "year", "country", "language"];
 
     var queryResolve;
-    
+
     //verify if the queries in empty and save as a object ex.: {title: "Die Leiden des jungen Werthers"}
     //also eliminate bad keys
-    if (typeof querie !== "undefined") {
+    if (typeof queries !== "undefined") {
       queryResolve = Object.fromEntries(
         Object.entries(queries).filter(
           ([key, value]) =>
