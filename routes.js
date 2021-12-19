6f077792cf9d108ec58da5d9e04c6e4b16e09248
vonweinkeller <caiankeller@icloud.com>
2021-12-19 10:30:51 -0300

    resolving git

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const BookController = require("./controllers/BookController");
-
-const routes = express.Router();
-
-routes.get("/getbooks", BookController.getBooks);
-
-module.exports = routes;
