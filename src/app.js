83f66a61ab7ccef442f43f7942985060f1f4c543
vonweinkeller <caiankardoso@gmail.com>
2022-04-08 13:22:56 -0300

    fixing uppercase in the files, can cause trouble in case sensitive systems

--- src/app.js
+++ src/app.js
@@ -16,6 +16,6 @@ app.use(routes)
 
 mongoose.connect(process.env.BD_URL)
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log(`We've taken off 🛫 on ${process.env.PORT || 3001} port`)
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`We've taken off 🛫 on ${process.env.PORT || 4000} port`)
 })
